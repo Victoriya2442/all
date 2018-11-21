22881fedefc5c0d46872f671665308617f485485
Victoriya <olishchukvv@gmail.com>
2018-11-21 19:00:16 +0200

    all

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $(".content").load("home.html"); 
+});
+
+function loadPage(page){
+    $(".content").load(page);
+}
+
+$(function(){
+    $(".content1").load("klas8.html"); 
+});
+
+function loadPage1(page){
+    $(".content1").load(page);
+}
+
